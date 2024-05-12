0d024cdfc5d01541e074e979077f08e151fa7105
mindula-madhuhansa <mimmindula@gmail.com>
2024-05-13 00:17:51 +0530

    add header

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,16 +1,15 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-
-import authors from "./authors";
-import books from "./books";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.route("/authors", authors);
-app.route("/books", books);
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello Next.js!",
+  });
+});
 
 export const GET = handle(app);
 export const POST = handle(app);
