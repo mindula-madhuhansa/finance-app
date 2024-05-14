b32a7751592feb8e4dfe411e3f7bf3b47095ce6c
mindula-madhuhansa <mimmindula@gmail.com>
2024-05-14 23:08:56 +0530

    add create account form

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  const { isOpen, onOpen } = useNewAccount();
+
+  return (
+    <div>
+      <Button onClick={onOpen}>Add an Account</Button>
+    </div>
+  );
 }
