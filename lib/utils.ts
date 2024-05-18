8e1e26a83bd0ec224e48644d8c8c310a0002d159
mindula-madhuhansa <mimmindula@gmail.com>
2024-05-18 15:47:05 +0530

    add data grid

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
-import { eachDayOfInterval, isSameDay } from "date-fns";
+import { eachDayOfInterval, format, isSameDay, subDays } from "date-fns";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -54,11 +54,56 @@ export function fillMissingDays(
     if (found) {
       return found;
     } else {
-      date: day;
-      income: 0;
-      expenses: 0;
+      return {
+        date: day,
+        income: 0,
+        expenses: 0,
+      };
     }
   });
 
   return transactionsByDay;
+}
+
+type Period = {
+  from: string | Date | undefined;
+  to: string | Date | undefined;
+};
+
+export function formatDateRange(period?: Period) {
+  const defaltTo = new Date();
+  const defaultFrom = subDays(defaltTo, 30);
+
+  if (!period?.from) {
+    return `${format(defaultFrom, "LLL dd")} - ${format(
+      defaltTo,
+      "LLL dd, y"
+    )}`;
+  }
+
+  if (period.to) {
+    return `${format(period.from, "LLL dd")} - ${format(
+      period.to,
+      "LLL dd, y"
+    )}`;
+  }
+
+  return format(period.from, "LLL dd, y");
+}
+
+export function formatPercentage(
+  value: number,
+  options: { addPrefix?: boolean } = {
+    addPrefix: false,
+  }
+) {
+  const result = new Intl.NumberFormat("en-US", {
+    style: "percent",
+  }).format(value / 100);
+
+  if (options.addPrefix && value > 0) {
+    return `+${result}`;
+  }
+
+  return result;
 }
